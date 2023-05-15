8c9730f4c0b3b06e2738002ceb04848b20857a3a
Ana Silva <annadasilva89@hotmail.com>
2023-05-14 21:47:10 -0300

    GET lists

---
+++ dslist/src/main/java/com/dev/dslist/repositories/GameListRepository.java
@@ -0,0 +1,9 @@
+package com.dev.dslist.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.dev.dslist.entities.GameList;
+
+public interface GameListRepository extends JpaRepository<GameList, Long> {
+
+}
